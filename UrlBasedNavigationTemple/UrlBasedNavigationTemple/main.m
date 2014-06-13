2910b4088adef522dea8df293c9e1366d6acde8d
flytonet <flytonet@163.com>
2014-06-13 17:42:23 +0800

    commit first for empty project

---
+++ UrlBasedNavigationTemple/UrlBasedNavigationTemple/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UrlBasedNavigationTemple
+//
+//  Created by 笑痴 on 14-6-13.
+//  Copyright (c) 2014年 SELF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
